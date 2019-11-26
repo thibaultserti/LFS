a75094b16748eef1a8ffd4fb036cdf0693d3cb33
Thibault Serti <thibault.serti@gmail.com>
2019-11-26 20:02:41 +0100

    Add tools directory

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # ------ PRÉREQUIS --------------
 echo "Bienvenue dans l'assistant d'installation de Linux From Scratch LFS"
 if [ `bash version-check.sh | tail -n 1 | cut -d ' ' -f3-` != "OK" ]; 
@@ -6,8 +7,9 @@ then
 echo "Le système hôte n'a pas les prérequis nécessaires ! Vous pouvez lancez la commande bash version-check.sh pour essayer de comprendre d'où vient le problème !"
 exit 1
 fi
-echo "Choisissez la partition d'installation de LFS"
+
 # ------ PARTITIONNEMENT ---------
+echo "Choisissez la partition d'installation de LFS"
 isNotOk=false
 while is_not_ok;
 do
@@ -46,3 +48,5 @@ fi
 
 popd
 
+# ------------- RÉPERTOIRE TOOLS ----------
+mkdir -v $LFS/tools
