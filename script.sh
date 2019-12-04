f2e0a024489d68da38a9885d319cd2cb6e85d04d
Thibault Serti <thibault.serti@gmail.com>
2019-12-04 16:32:41 +0100

    Update wget-list

--- script.sh
+++ script.sh
@@ -4,8 +4,8 @@
 echo "Bienvenue dans l'assistant d'installation de Linux From Scratch LFS"
 if [ `bash version-check.sh | tail -n 1 | cut -d ' ' -f3-` != "OK" ]; 
 then
-echo "Le système hôte n'a pas les prérequis nécessaires ! Vous pouvez lancez la commande bash version-check.sh pour essayer de comprendre d'où vient le problème !"
-exit 1
+    echo "Le système hôte n'a pas les prérequis nécessaires ! Vous pouvez lancez la commande bash version-check.sh pour essayer de comprendre d'où vient le problème !";
+    exit 1;
 fi
 
 bash version-check.sh
@@ -13,7 +13,7 @@ echo "Êtes vous sûr de vouloir continuer (o/N) ?"
 read val
 if [ $val != "o" ];
 then
-exit 1
+    exit 1
 fi
 
 # ------ PARTITIONNEMENT ---------
@@ -21,16 +21,15 @@ echo "Choisissez la partition d'installation de LFS"
 
 while [ true ];
 do
-sudo fdisk -l | tail -n +10 | head -n -2 
-echo "Quelle partition voulez vous utiliser pour LFS ? (ATTENTION : toutes les données déjà présentes sur le disque seront effacées ! (Taper /dev/sdxx)"
-read partition
-if ! [[ $partition =~ /dev/sd[a-f][1-9] ]];
-then
-echo "Partition invalide !";
-else
-break;
-fi
-
+    sudo fdisk -l | tail -n +10 | head -n -2 
+    echo "Quelle partition voulez vous utiliser pour LFS ? (ATTENTION : toutes les données déjà présentes sur le disque seront effacées ! (Taper /dev/sdxx)"
+    read partition
+    if ! [[ $partition =~ /dev/sd[a-f][1-9] ]];
+    then
+        echo "Partition invalide !";
+    else
+        break;
+    fi
 done
 
 
@@ -52,7 +51,8 @@ chmod -v a+wt $LFS/sources
 echo "Téléchargement en cours ... "
 wget --input-file=wget-list --continue --directory-prefix=$LFS/sources
 pushd $LFS/sources
-if [ md5sum -c md5sums | tail -n 1 != "Success" ] && [ md5sum -c md5sums | tail -n 1 == "Réussi" ]
+if [ md5sum -c md5sums | tail -n 1 != "Success" ] && [ md5sum -c md5sums | tail -n 1 == "Réussi" ];
+then 
     echo "Les sommes de contrôles md5 ne correspondent pas !" ;
     popd ;
     exit 1
