a2d71e342baff8a9958fb5ecfd770ac8f92797d6
Thibault Serti <thibault.serti@gmail.com>
2019-12-11 17:15:16 +0100

    Add Libstdc++ compilation

--- script.sh
+++ script.sh
@@ -228,8 +228,10 @@ rm -rf linux-5.2.8/
 
 # GLIBC
 
-su lfs -c 'tar -xf glibc-2.30.tar.xz'
+echo "Compilation de Glibc ..."
 
+su lfs -c 'tar -xf glibc-2.30.tar.xz'
+cd glibc-2.30/
 su lfs -c 'mkdir -v build'
 cd build/
 
@@ -260,3 +262,25 @@ rm -v dummy.c a.out
 
 cd $LFS/sources/
 rm -rf glibc-2.30/
+
+# LIBSTDC++
+
+echo "Compilation de Libstdc++ ..."
+su lfs -c 'tar -xf gcc-9.2.0.tar.xz'
+cd gcc-9.2.0/
+
+su lfs -c 'mkdir -v build'
+cd build/
+
+su lfs -c '
+    ../libstdc++-v3/configure
+    --host=$LFS_TGT                 
+    --prefix=/tools                 
+    --disable-multilib              
+    --disable-nls                   
+    --disable-libstdcxx-threads     
+    --disable-libstdcxx-pch         
+    --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/9.2.0
+    && make
+    && make install'
+    
